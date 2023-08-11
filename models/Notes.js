4169b75fea876c4913e6efa83d76a58656deb341
vikram <vikramjeet.p@stratagile.com>
2023-08-11 15:13:35 +0530

    Create user by promise then & catch

---
+++ models/Notes.js
@@ -0,0 +1,24 @@
+//import mongoose from 'mongoose';
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const NotesSchema = new Schema({
+    title:{
+        type:String,
+        required:true
+    },
+    description:{
+        type:String,
+        required:true,
+    },
+    tag:{
+        type:String,
+        default:'general'
+    },
+    date:{
+        type:date,
+        default:Date.now
+    }
+});
+
+module.exports = mongoose.model('notes',NotesSchema);
