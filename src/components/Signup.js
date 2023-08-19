247b892ae67d126f5ed14734334bfeb63f91c752
vikram <vikramjeet.p@stratagile.com>
2023-08-19 19:31:18 +0530

    Add signup form and bind up create user

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,9 +1,73 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useNavigate } from "react-router-dom";
 
 export default function Signup() {
+
+  const navigate = useNavigate();
+
+  const [signup, setSignup] = useState({ name: "", email: "", password: "", cpassword: '' });
+
+  const onChange = (e) => {
+    const { name, value } = e.target;
+    setSignup({ ...signup, [name]: value });
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    let { password, cpassword } = signup;
+    try {
+
+      if (password === cpassword) {
+        const res = await fetch('http://localhost:5000/api/auth/createuser', {
+          method: 'POST',
+          headers: {
+            'Content-type': 'application/json'
+          },
+          body: JSON.stringify({
+            name: signup.name,
+            email: signup.email,
+            password: signup.password
+          })
+        })
+
+        const jsondata = await res.json();
+        console.log(jsondata)
+        if (jsondata.success) {
+          localStorage.setItem('token', jsondata.authtoken)
+          navigate('/');
+        } else {
+          alert("invalid credential");
+        }
+      }
+
+    } catch (error) {
+      console.log(error.message);
+    }
+
+  }
+
   return (
-    <div>
-       i m singup
+    <div className='container'>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label htmlFor="name">Name</label>
+          <input type="text" className="form-control" id="name" onChange={onChange} name="name" required={true} placeholder="Enter Name" />
+        </div>
+        <div className="form-group">
+          <label htmlFor="email">Email address</label>
+          <input type="email" className="form-control" id="email" onChange={onChange} name="email" required={true} placeholder="Enter email" />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password</label>
+          <input type="password" className="form-control" id="password" onChange={onChange} required={true} minLength={5} name="password" placeholder="Password" autoComplete='On' />
+        </div>
+        <div className="form-group">
+          <label htmlFor="exampleInputPassword1">Confirm Password</label>
+          <input type="password" className="form-control" id="cpassword" onChange={onChange} required={true} minLength={5} name="cpassword" placeholder="Password" autoComplete='On' />
+        </div>
+        <button type="submit" className="btn btn-primary">Sign Up</button>
+      </form>
     </div>
   )
 }
